e5d81f96f1e69e37ddfeb21a795d9422d3187db4
MKCocoon <jychew@ualberta.ca>
2021-03-15 00:43:27 -0600

    Created ProfileActivity and GUI for MainActivity and linked them together

---
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/ProfileActivity.java
@@ -0,0 +1,26 @@
+package com.example.mogulmoves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+
+    public void goBack (View view)
+    {
+        finish();
+    }
+
+
+
+}
