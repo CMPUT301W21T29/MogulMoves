70fb318df4d45752a1881cf976c9de479a3dc486
GoodPiggy <joshua@jarman.ca>
2021-03-15 22:06:02 -0600

    _

---
+++ app/src/main/java/com/example/mogulmoves/IntegerCountTrial.java
@@ -0,0 +1,13 @@
+package com.example.mogulmoves;
+
+public class IntegerCountTrial extends NonNegativeCountTrial{
+
+    public IntegerCountTrial(Experimenter experimenter, int count) {
+        super(experimenter, count);
+    }
+
+    public void decriment(){
+        count--;
+    }
+
+}
