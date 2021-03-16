5a0b24c518130815c37c30f52fc7b914989632ac
GoodPiggy <joshua@jarman.ca>
2021-03-15 23:33:52 -0600

    finally able to start codeing

--- app/src/main/java/com/example/mogulmoves/MainActivity.java
+++ app/src/main/java/com/example/mogulmoves/MainActivity.java
@@ -44,47 +44,6 @@ public class MainActivity extends AppCompatActivity {
     private void initQAModule() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        Spinner expSpinner = findViewById(R.id.spinner);
-
-        EditText txtSubject = findViewById(R.id.txtSubject);
-        EditText txtEmail = findViewById(R.id.txtEmail);
-        EditText txtQuestion = findViewById(R.id.txtQuestion);
-
-        Button btnQuestionSubmit = findViewById(R.id.btnSubmitQuestion);
-        btnQuestionSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String experiment = expSpinner.getSelectedItem().toString();
-                String subject = txtSubject.getText().toString();
-                String email = txtEmail.getText().toString();
-                String question = txtQuestion.getText().toString();
-
-                Map<String, Object> docData = new HashMap<>();
-                docData.put("subject", subject);
-                docData.put("experiment", experiment);
-                docData.put("email", email);
-                docData.put("question", question);
-
-                db.collection("questions")
-                        .add(docData)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                System.out.println("Question Added");
-                                txtSubject.setText("");
-                                txtEmail.setText("");
-                                txtQuestion.setText("");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                System.out.println("Question Addition Failed");
-                            }
-                        });
-
-            }
-        });
       
     }
 
