9ec90c284d352b7d4664276da12e47a5f171491f
DecklynKern <71661150+DecklynKern@users.noreply.github.com>
2021-03-16 12:37:10 -0600

    add javadocs to all classes

--- MogulMoves/app/src/main/java/com/example/mogulmoves/ObjectContext.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/ObjectContext.java
@@ -2,6 +2,9 @@ package com.example.mogulmoves;
 
 import java.util.ArrayList;
 
+/**
+ * Class to contain methods and attributes that can be accessed globally.
+ */
 public class ObjectContext {
 
     public static int nextId;
@@ -11,6 +14,12 @@ public class ObjectContext {
     public static ArrayList<Trial> trials = new ArrayList<>();
     public static ArrayList<Question> questions = new ArrayList<>();
 
+    /**
+     * Returns the object with the given id, or null if that object does not exist.
+     *
+     * @param id the id of an object
+     * @return the corresponding SavedObject
+     */
     public static SavedObject getObjectById(int id) {
 
         for(User user: users) {
@@ -37,18 +46,31 @@ public class ObjectContext {
             }
         }
 
-        return null; // should not happen
+        return null; // should not happen hopefully
 
     }
 
+    /**
+     * Adds an experiment to the list of experiments.
+     * Pushes its data to the database, will cause refresh of all experiments.
+     *
+     * @param experiment an Experiment object to add
+     */
     public static void addExperiment(Experiment experiment) {
+
         ExperimentSerializer serializer = new ExperimentSerializer();
         DatabaseHandler.pushData("experiments", "" + experiment.getId(),
                 serializer.toData(experiment));
 
     }
 
-    public void addUser(User user) {
+    /**
+     * Adds a user to the list of users.
+     * Pushes its data to the database, will cause refresh of all users.
+     *
+     * @param user a User object to add
+     */
+    public static void addUser(User user) {
 
         UserSerializer serializer = new UserSerializer();
         DatabaseHandler.pushData("users", "" + user.getId(),
@@ -56,7 +78,14 @@ public class ObjectContext {
 
     }
 
-    public void addTrial(Trial trial, Experiment experiment) {
+    /**
+     * Adds a trial to the list of trials, and adds it to the experiment's list of trials.
+     * Pushes its data to the database, will cause refresh of all trials.
+     *
+     * @param trial a Trial object to add
+     * @param experiment the parent experiment to add the trial to
+     */
+    public static void addTrial(Trial trial, Experiment experiment) {
 
         experiment.addTrial(trial.getId());
 
@@ -65,7 +94,14 @@ public class ObjectContext {
                 serializer.toData(trial));
     }
 
-    public void addReply(Message message, Question question) {
+    /**
+     * Adds a message to the list of messages, and adds it to the main question's list of replies.
+     * Pushes its data to the database, will cause refresh of all messages.
+     *
+     * @param message a Message object to add
+     * @param question the parent question to add the reply to
+     */
+    public static void addReply(Message message, Question question) {
 
         question.addReply(message.getId());
 
@@ -74,7 +110,14 @@ public class ObjectContext {
                 serializer.toData(message));
     }
 
-    public void addQuestion(Question question) {
+
+    /**
+     * Adds a question to the list of questions.
+     * Pushes its data to the database, will cause refresh of all messages.
+     *
+     * @param question a Question object to add
+     */
+    public static void addQuestion(Question question) {
 
         MessageSerializer serializer = new MessageSerializer();
         DatabaseHandler.pushData("messages", "" + question.getId(),
