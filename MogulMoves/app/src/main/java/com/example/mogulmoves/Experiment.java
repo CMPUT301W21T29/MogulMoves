8498767af2ad998162bc182fc6391ddf3ac673af
WhySoooSerious <haoyangcheng01@gmail.com>
2021-03-16 22:24:48 -0600

    comparing and adding

--- MogulMoves/app/src/main/java/com/example/mogulmoves/Experiment.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/Experiment.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Abstract class to represent an experiment and all of its data.
  */
-public abstract class Experiment extends SavedObject {
+public abstract class Experiment extends SavedObject implements GeoExperiment{
 
     private boolean active = true;
     private boolean locationRequired = false;
