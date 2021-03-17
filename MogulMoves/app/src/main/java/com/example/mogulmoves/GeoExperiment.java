f0c031529a172e34486c846d3c900710b42f449e
WhySoooSerious <haoyangcheng01@gmail.com>
2021-03-16 21:39:27 -0600

    editing conflict

--- MogulMoves/app/src/main/java/com/example/mogulmoves/GeoExperiment.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/GeoExperiment.java
@@ -7,4 +7,5 @@ public interface GeoExperiment {
     public boolean getLocationRequired();
     public void setLocationRequired(boolean locationRequired, User user) throws IOException;
     public String GeoExperimentWarning(User user);
+    public ArrayList<Location> getAllLocations() throws IOException;
 }
