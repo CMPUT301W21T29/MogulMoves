4c254b3d93bed593654131b61bc223b4d81b5a20
WhySoooSerious <haoyangcheng01@gmail.com>
2021-03-16 21:50:50 -0600

    comparing and adding

--- MogulMoves/app/src/main/java/com/example/mogulmoves/GeoTrial.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/GeoTrial.java
@@ -6,4 +6,5 @@ import java.io.IOException;
 public interface GeoTrial {
     public void addExperimenterGeo();
     public Location getExperimenterGeo() throws IOException;
+    public boolean setGeoRequired();
 }
