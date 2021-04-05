3c80e6c5fac25de093c8bd4b64dccd32d5f0fcc5
WhySoooSerious <haoyangcheng01@gmail.com>
2021-04-04 19:04:59 -0600

    adding

--- MogulMoves/app/src/main/java/com/example/mogulmoves/MapFragment.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/MapFragment.java
@@ -24,16 +24,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapFragment extends Fragment {
     /*
 
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-
     /*
     public static MapFragment newInstance(String param1, String param2) {
         MapFragment fragment = new MapFragment();
