2d45c530f428aabadbdd10a6bb8c4583024caba7
WhySoooSerious <haoyangcheng01@gmail.com>
2021-04-04 19:00:29 -0600

    adding

--- MogulMoves/app/src/main/java/com/example/mogulmoves/MapFragment.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/MapFragment.java
@@ -23,7 +23,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class MapFragment extends Fragment {
     /*
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
