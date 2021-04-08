d87a27ad291d385fa867cf65c1ca8540beb38cdc
MKCocoon <60154827+MKCocoon@users.noreply.github.com>
2021-04-08 11:53:47 -0600

    moved experiment settings to new fragment activity

---
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/CustomSettingsDialog.java
@@ -0,0 +1,26 @@
+package com.example.mogulmoves;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+//Custom dialog class for EditCountTrialFragment.
+
+public class CustomSettingsDialog extends AlertDialog {
+    private Button negative;
+
+    public CustomSettingsDialog(Context context, View view) {
+        super(context);
+        setContentView(view);
+        negative = (Button)findViewById(R.id.back_button);
+    }
+
+    public void setNegativeButton(String buttonText, View.OnClickListener listener) {
+        negative.setText(buttonText);
+        negative.setOnClickListener(listener);
+        negative.setVisibility(View.VISIBLE);
+    }
+
+}
