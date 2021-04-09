732a9f2b989c3559ca831556257913a85b48e7bc
WhySoooSerious <haoyangcheng01@gmail.com>
2021-04-09 01:59:20 -0600

    add/delete

--- MogulMoves/app/src/main/java/com/example/mogulmoves/MapAdapter.java
+++ MogulMoves/app/src/main/java/com/example/mogulmoves/MapAdapter.java
@@ -6,3 +6,51 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+/**
+ *Adapter to display a map of trial locations.
+ */
+public class MapAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_adapter);
+        Intent intent = getIntent();
+        int expId = Integer.parseInt(intent.getStringExtra("whichExperiment"));
+        Experiment exp = ObjectContext.getExperimentById(expId);
+
+        //Initialize fragment
+        if (exp instanceof BinomialExperiment) {
+            Fragment fragment = new MapFragment((BinomialExperiment) exp);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.map_frame_layout, fragment)
+                    .commit();
+        }
+        else if (exp instanceof IntegerCountExperiment) {
+            Fragment fragment = new MapFragment((IntegerCountExperiment) exp);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.map_frame_layout, fragment)
+                    .commit();
+        }
+        else if (exp instanceof NonNegativeCountExperiment) {
+            Fragment fragment = new MapFragment((NonNegativeCountExperiment) exp);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.map_frame_layout, fragment)
+                    .commit();
+        }
+        else if (exp instanceof MeasureExperiment) {
+            Fragment fragment = new MapFragment((MeasureExperiment) exp);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.map_frame_layout, fragment)
+                    .commit();
+        }
+    }
+}
